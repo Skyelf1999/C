74eeb517643d8b47e718d16412c7dd1dab2efa69
Di Shihao <skyelfxxx@126.com>
2023-01-13 15:13:31 +0800

    多维Vector

--- C++/testProgram/test.cpp
+++ C++/testProgram/test.cpp
@@ -7,11 +7,36 @@ using namespace std;
 
 int main()
 {
-    vector<int> arr(2);
-    arr[0] = 1;
-    arr[1] = 3;
-    arr.push_back(5);
-    cout<<arr[2]<<endl;
+    int r,c;
+
+    vector<vector<int>> mat(3);
+    for(int i=0;i<3;i++)
+    {
+        vector<int> row(i+1);
+        for(int j=0;j<i+1;j++) row[j] = i+j;
+        mat[i] = row;
+    }
+    for(int i=0;i<3;i++)
+    {
+        vector<int>::iterator it;
+        for(it=mat[i].begin();it!=mat[i].end();it++)
+            cout<<*it<<endl;
+        cout<<"\n"<<endl;
+    }
+
+    // vector<vector<int>> ret(r);
+
+    // for(int i=0;i<r;i++)
+    // {
+    //     vector<int> row(c);
+    //     for(int j=0;j<c;j++)
+    //     {
+    //         int index = i*r+c;
+    //         row.push_back(mat[index/m][index-index/m*n]);
+    //     }
+    //     ret.push_back(row);
+    // }
+
 
     return 0;
 }
